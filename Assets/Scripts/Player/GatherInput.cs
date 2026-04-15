97478d3a22d49efb2993a875bc9a952777f47703
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2026-04-15 19:04:37 +0200

    Add player input controls (#2)
    * Add Player object
    
    * Insert player actions controls
    
    * Add unity files
    
    * Add script to gather input actions for the player

---
+++ Assets/Scripts/Player/GatherInput.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GatherInput : MonoBehaviour
+{
+    public PlayerInput playerInput;
+
+    public InputActionReference jumpActionRef;
+    public InputActionReference moveActionRef;
+
+    private InputActionMap playerMap;
+    private InputActionMap uiMap;
+
+    private float horizontalInput;
+
+    private void OnEnable()
+    {
+        jumpActionRef.action.performed += TryToJump;
+        jumpActionRef.action.canceled += StopJump;
+    }
+
+    private void OnDisable()
+    {
+        jumpActionRef.action.performed -= TryToJump;
+        jumpActionRef.action.canceled -= StopJump;
+        playerMap.Disable();
+    }
+
+    private void TryToJump(InputAction.CallbackContext value)
+    {
+        Debug.Log("JUMP");
+    }
+
+    private void StopJump(InputAction.CallbackContext value)
+    {
+        Debug.Log("STOP JUMP");
+    }
+
+    void Start()
+    {
+        playerMap = playerInput.actions.FindActionMap("Player");
+        uiMap = playerInput.actions.FindActionMap("UI");
+        playerMap.Enable();
+    }
+
+    void Update()
+    {
+        horizontalInput = moveActionRef.action.ReadValue<float>();
+        Debug.Log("Horizontal Input = " + horizontalInput);
+    }
+}
