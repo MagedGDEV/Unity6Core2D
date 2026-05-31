559781b086cec55cece8515748eeec252cac3fa5
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2026-05-31 16:55:10 +0300

    Comment for infinity parallaxing

--- Assets/Scripts/Parallax/ParallaxEffect.cs
+++ Assets/Scripts/Parallax/ParallaxEffect.cs
@@ -20,5 +20,17 @@ public class ParallaxEffect : MonoBehaviour
         deltaMovement = cam.transform.position - lastCameraPosition;
         transform.position += new Vector3(deltaMovement.x * parallaxValue.x, deltaMovement.y * parallaxValue.y, 0);
         lastCameraPosition = cam.transform.position;
+        
+        // To make infinity parallaxing
+        // You need to add child to the object with the same design
+        // So the player cant see the changes
+        // if (cam.transform.position.x - transform.position.x >= spriteLength)
+        // {
+        //     transform.position = new Vector3(cam.transform.position.x + spriteLength, transform.position.y);
+        // }
+        // else if (cam.transform.position.x - transform.position.x <= -spriteLength)
+        // {
+        //     transform.position = new Vector3(cam.transform.position.x - spriteLength, transform.position.y);
+        // }
     }
 }
