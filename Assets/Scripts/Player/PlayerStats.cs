cc5f6a5c3b36beafa747ecb02d84b29f691e2702
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2026-06-06 18:23:01 +0300

    Add health bar

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     [SerializeField] private Player player;
+    [SerializeField] private HealthBarControl healthBarControl;
     [SerializeField] private float maxHealth;
     private float currentHealth;
     
     void Start()
     {
         currentHealth = maxHealth;
+        healthBarControl.SetSliderValue(currentHealth, maxHealth);
     }
 
     public void DamagePlayer(float damage)
     {
         currentHealth -= damage;
+        healthBarControl.SetSliderValue(currentHealth, maxHealth);
         if (currentHealth <= 0)
         {
             if (player.stateMachine.currentState != PlayerStates.State.KnockBack)
