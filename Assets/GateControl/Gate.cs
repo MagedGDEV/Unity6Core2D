338b8953e3124ccaa8798a293b1e14525769e47c
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2026-06-16 20:30:32 +0300

    Update unity files

---
+++ Assets/GateControl/Gate.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField] private string levelToLoad;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.GetComponent<Player>();
+            player.gatherInput.DisablePlayerInput();
+            player.physicsControl.ResetVelocity();
+            LevelManager.instance.LoadLevel(levelToLoad);
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+}
